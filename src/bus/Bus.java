bf7ce17db0a7bd94416eb1a7e9b556621a88b088
wiandamumtaz <wianda_mumtaz_29rpl@student.smktelkom-mlg.sch.id>
2021-01-18 17:25:37 +0700

    BUS

---
+++ src/bus/Bus.java
@@ -0,0 +1,14 @@
+package bus;
+public class Bus {
+    public static void main(String[] args) {
+        
+    }
+    public int Penumpang;
+    public int MaxPenumpang;
+    
+    public void cetak(){
+        System.out.println("Penumpang Bus Sekarang Adalah :"+Penumpang);
+        System.out.println("Penumpang Maksimum seharusnya adalah "+MaxPenumpang);
+    }
+    
+}
